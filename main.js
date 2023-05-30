cf4bb0af3524d5e68f1c1a85e599faae495639a2
Akshat Kumar <115036884+k-akshat@users.noreply.github.com>
2023-05-31 03:45:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+document.addEventListener("keypress",function(){
+    var randomNumber1 = Math.floor(6*Math.random() + 1);
+    var randomNumber2 = Math.floor(6*Math.random() + 1);
+    document.querySelectorAll("img")[0].setAttribute("src" , "images/dice"+randomNumber1+".png");
+    document.querySelectorAll("img")[1].setAttribute("src" , "images/dice"+randomNumber2+".png");
+
+    if(randomNumber1 > randomNumber2){
+        document.querySelector("h1").innerHTML = "🚩 Player 1 Wins !!";
+    }
+    else if (randomNumber1 < randomNumber2){
+        document.querySelector("h1").innerHTML = "Player 2 Wins !! 🚩";
+    }
+    else{
+        document.querySelector("h1").innerHTML = "Draw";
+    }
+});
